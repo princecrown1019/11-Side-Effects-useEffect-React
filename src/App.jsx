ec30b938ae9f8b97a20c6361fd2f23ad85b692b6
princecrown1019 <princecrown1019@gmail.com>
2025-06-10 15:39:59 +0300

    geolocation side effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -10,12 +10,16 @@ import { sortPlacesByDistance } from './loc.js';
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude)
-  });
-
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude)
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+  
   function handleStartRemovePlace(id) {
     modal.current.open();
     selectedPlace.current = id;
@@ -68,7 +72,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting available places..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
